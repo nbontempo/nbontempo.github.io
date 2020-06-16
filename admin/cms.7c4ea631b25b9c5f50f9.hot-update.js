46314c050371b573953af88eafc78dcb5cf8ad8d
Nicolas Bontempo <nickbomtempo@hotmail.com>
2020-06-15 23:58:02 -0300

    Updates

---
+++ admin/cms.7c4ea631b25b9c5f50f9.hot-update.js
@@ -0,0 +1,86 @@
+webpackHotUpdate("cms",{
+
+/***/ "./src/cms/preview-templates/page-preview.js":
+/*!***************************************************!*\
+  !*** ./src/cms/preview-templates/page-preview.js ***!
+  \***************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _this = undefined,
+    _jsxFileName = "D:\\1-Source\\nickbom.github.io\\src\\cms\\preview-templates\\page-preview.js";
+
+(function () {
+  var enterModule = typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal.enterModule : undefined;
+  enterModule && enterModule(module);
+})();
+
+var __signature__ = typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal.default.signature : function (a) {
+  return a;
+};
+
+
+
+var PagePreview = function PagePreview(_ref) {
+  var entry = _ref.entry,
+      widgetFor = _ref.widgetFor;
+  var body = widgetFor("body");
+  var title = entry.getIn(["data", "title"]);
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "page",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16,
+      columnNumber: 5
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    className: "page__title",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17,
+      columnNumber: 7
+    }
+  }, title), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "page__body",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18,
+      columnNumber: 7
+    }
+  }, body));
+};
+
+var _default = PagePreview;
+/* harmony default export */ __webpack_exports__["default"] = (_default);
+;
+
+(function () {
+  var reactHotLoader = typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal.default : undefined;
+
+  if (!reactHotLoader) {
+    return;
+  }
+
+  reactHotLoader.register(PagePreview, "PagePreview", "D:\\1-Source\\nickbom.github.io\\src\\cms\\preview-templates\\page-preview.js");
+  reactHotLoader.register(_default, "default", "D:\\1-Source\\nickbom.github.io\\src\\cms\\preview-templates\\page-preview.js");
+})();
+
+;
+
+(function () {
+  var leaveModule = typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal.leaveModule : undefined;
+  leaveModule && leaveModule(module);
+})();
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../../../node_modules/webpack/buildin/harmony-module.js */ "./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=cms.7c4ea631b25b9c5f50f9.hot-update.js.map
